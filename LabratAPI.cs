7f1426533cf3766971c62308c7e5a6bc7f6238b5
VirtualBrightPlayz <lego.trains.b@gmail.com>
2022-01-01 03:30:33 -0700

    text commands

--- LabratAPI.cs
+++ LabratAPI.cs
@@ -15,6 +15,18 @@ public static class LabratAPI
         return await response.Content.ReadAsStringAsync();
     }
 
+    public static async Task<string> Text(string text, float time = 10f)
+    {
+        string str = string.Empty;
+        for (int i = 0; i < text.Length; i++)
+        {
+            str += Uri.HexEscape(text[i]);
+        }
+        HttpClient http = new HttpClient();
+        var response = await http.SendAsync(new HttpRequestMessage(HttpMethod.Get, $"http://127.0.0.1:8090/text?text={str}&time={time}"));
+        return await response.Content.ReadAsStringAsync();
+    }
+
     public static async Task<string> Handle(string arg)
     {
         var spl = arg.Split(' ');
@@ -38,6 +50,16 @@ public static class LabratAPI
             case "die":
             {
                 return await Die();
+            }
+            case "text":
+            {
+                string str = string.Empty;
+                for (int i = 1; i < spl.Length; i++)
+                {
+                    str += spl[i] + " ";
+                }
+                str = str.Trim();
+                return await Text(str);
             }
         }
         return string.Empty;
---
+++ Tests/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    public static Task Main(string[] args) => MainAsync();
+    
+    public static async Task MainAsync()
+    {
+        await LabratAPI.Text("https://google.com\nh");
+    }
+}
